e446ac126d716d26433b6ca1e22d9ef1c19ea3ee
pavan-0328 <pavansaikumarreddy354@gmail.com>
2023-06-08 22:50:16 +0530

    Update nxtPermutation

--- nxtPermutationLettcode.cpp
+++ nxtPermutationLettcode.cpp
@@ -1,28 +1,27 @@
-#include <bits/stdc++.h> 
+class Solution {
+public:
 
 int find_nxtbig(vector<int> &arr, int from){
   int idx = arr.size()-1; // if from element itself is smaller than the rest of arrray elements to its right.
   for(int i=from+1;i<arr.size();i++){
-      if(arr[from]>=arr[i]){ //what if all the elements are equal 
+      if(arr[from]>=arr[i]){
           idx = i-1;
           break;
       }
   }
   return idx;
 }
-vector<int> nextPermutation(vector<int> &permutation, int n)
-{
-    //  Write your code here.
-    vector<int> arr = permutation;
+    void nextPermutation(vector<int>& arr) {
+        
+        int n=arr.size();
     for(int i=n-1;i>0;i--){
         if(arr[i] > arr[i-1]){
             int nxtbig_indx = find_nxtbig(arr, i-1);
             swap(arr[nxtbig_indx],arr[i-1]);
             reverse(arr.begin() + i,arr.end());
-            return arr;
+            return;
         }
     }
     sort(arr.begin(),arr.end());
-    return arr;
-
-}
+    }
+};
